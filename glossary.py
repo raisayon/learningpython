206fa150e0b98fc6caa251fcf1cc5b369f210c47
raisayon <59853083+raisayon@users.noreply.github.com>
2020-07-30 18:31:12 -0700

    Update glossary.py

--- glossary.py
+++ glossary.py
@@ -1,2 +1,6 @@
 Collection : Grouping multiple items together and storing them with a single name called a variable
 List : simple collection that groups pieces of data together in  a certain order and assigns the collection a name
+Iteration : Repeats the same procedure multiple times until it reaches a specified endpoint
+Loop : code that iterates,moving from beginning to end of the process,then starting over
+for : specifies a variable name that we can use in each iteration of the loop to reference the current value
+in : indicates that what follows is the set of values that we want to iterate through
