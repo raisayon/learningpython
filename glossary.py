0dbaa33b9666cfdbc422a0f148879c799326176d
raisayon <59853083+raisayon@users.noreply.github.com>
2020-07-30 17:26:53 -0700

    Create glossary.py

---
+++ glossary.py
@@ -0,0 +1,2 @@
+Collection : Grouping multiple items together and storing them with a single name called a variable
+List : simple collection that groups pieces of data together in  a certain order and assigns the collection a name
